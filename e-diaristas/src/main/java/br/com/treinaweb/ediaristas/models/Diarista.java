8af71c295267c98a84992f8a0c61222caf4a4d97
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2021-11-08 20:05:15 -0800

    Remove lombok

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/models/Diarista.java
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/models/Diarista.java
@@ -6,21 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Diarista {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(nullable = false, length = 100)
@@ -49,8 +39,8 @@ public class Diarista {
 
     @Column(nullable = false, length = 8)
     private String cep;
-    @Column(nullable = false)
 
+    @Column(nullable = false)
     private String cidade;
 
     @Column(nullable = false, length = 2)
@@ -58,4 +48,149 @@ public class Diarista {
 
     @Column(nullable = false)
     private String codigoIbge;
+
+    public Diarista() {
+    }
+
+    public Diarista(Long id, String nomeCompleto, String cpf, String email, String telefone, String logradouro,
+            String numero, String bairro, String complemento, String cep, String cidade, String estado,
+            String codigoIbge) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.complemento = complemento;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.codigoIbge = codigoIbge;
+    }
+
+    public Diarista(String nomeCompleto, String cpf, String email, String telefone, String logradouro, String numero,
+            String bairro, String complemento, String cep, String cidade, String estado, String codigoIbge) {
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.complemento = complemento;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.codigoIbge = codigoIbge;
+    }
+
+    // #region Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCodigoIbge() {
+        return codigoIbge;
+    }
+
+    public void setCodigoIbge(String codigoIbge) {
+        this.codigoIbge = codigoIbge;
+    }
+    // #endregion
+
 }
