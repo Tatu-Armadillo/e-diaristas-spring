2feb2577962d5dffbf83047973c34f50f11496bb
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2021-11-16 11:53:31 -0400

    api connection in front-and next-react

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/config/CorsConfig.java
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/config/CorsConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/ediaristas*/*").allowedMethods("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
     }
 }
